bd448e245fa7c3a6888f131f58412aa8720177a9
Lionking93 <leo.kallonen@gmail.com>
2019-02-22 14:16:32 +0200

    osa 5 t5.2

--- src/App.js
+++ src/App.js
@@ -26,6 +26,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedInUserJSON = window.localStorage.getItem('loggedInBloglistUser')
+    if (loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON)
+      setUser(user)
+    }
+  })
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -33,6 +41,10 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })  
+
+      window.localStorage.setItem(
+        'loggedInBloglistUser', JSON.stringify(user)
+      )
 
       setUser(user)
       setUsername('')
@@ -43,6 +55,11 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedInBloglistUser')
+    setUser(null)
   }
 
   const showErrorMessage = () => {
@@ -72,6 +89,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in</p>
+      <button onClick={handleLogout}>Log out</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}    
