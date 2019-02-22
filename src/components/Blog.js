9776978d91ca533acad1388dd000caa8f81a8c64
Lionking93 <leo.kallonen@gmail.com>
2019-02-22 17:12:25 +0200

    osa 5 t5.6

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    border: '2px solid #ce6204',
+    borderRadius: '5px',
+    padding: '10px',
+    marginTop: '5px',
+    marginBottom: '5px',
+    background: '#fcf5db'
+  }
+
+  const [ showFullBlog, setShowFullBlog ] = useState(false)
+
+  const toggleShowFullBlog = () => {
+    setShowFullBlog(!showFullBlog)
+  }
+
+  const fullBlogStyle = { display: showFullBlog ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={toggleShowFullBlog}>{blog.title} {blog.author}</div>
+      <div style={fullBlogStyle}>
+        <div>{blog.url}</div>
+        <div>{blog.likes} likes <button>like</button></div>
+        <div>added by {blog.user.name}</div>
+      </div>
+    </div>
+)}
 
 export default Blog
